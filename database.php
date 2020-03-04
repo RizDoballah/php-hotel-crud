153b505151d59b76a1c12ce8df60d5fbf2fe0310
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-04 15:53:25 +0100

    database

---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "hotel";
+  // Connect
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+
+  if ($conn && $conn->connect_error) {
+    echo "Connexion Error: " . $conn->connect_error; die();
+  }
+
+  $sql = "SELECT * FROM stanze";
+  $result = $conn->query($sql);
+  if ($result && $result->num_rows > 0) {
+  // output data of each row
+    $rooms = [];
+    while($row = $result->fetch_assoc()) {
+      $rooms = $row;
+    }
+
+  } elseif ($result) {
+  echo "0 results";
+  } else {
+  echo "query error";
+  }
+  $conn->close();
+
+?>
