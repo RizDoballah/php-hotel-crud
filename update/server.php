0e721478d4530399e524354625140675b7d04f7d
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-09 16:26:48 +0100

    update

--- update/server.php
+++ update/server.php
@@ -1,17 +1,41 @@
-<?php include 'database.php';
-if(empty($_POST['id'])) {
-  die('NO ID');
-}
-  $idRoom = $_POST['id'];
+<?php
+  include_once __DIR__ . '/../env.php';
+  include __DIR__ .'/../database.php';
 
-  $sql = "UPDATE stanze SET room_number ='400' WHERE `id` = $idRoom";
+  if (empty($post['id'])) {
+    die('id non inserito');
+  }
+  if (empty($post['room_number'])) {
+    die('numero di stanza non inserito');
+  }
+  if (empty($post['floor'])) {
+    die('piano di stanza non inserito');
+  }
+  if (empty($post['beds'])) {
+    die('numero letti non inserito');
+  }
+
+  $roomId = $post['id'];
+  $roomNumber = $post['room_number'];
+  $floor = $post['floor'];
+  $beds = $post['beds'];
+
+  $sql= "SELECT * from `stanze` WHERE `id` = $roomId";
   $result = $conn->query($sql);
+
   if ($result && $result->num_rows > 0) {
     $room = $result->fetch_assoc();
-
-  } elseif ($result) {
-  echo "0 results";
   } else {
-  echo "query error";
+    die('id non esistente');
   }
-  $conn->close();
+
+  $sql = "UPDATE `stanze` SET `room_number` = $roomNumber, `beds` = $beds, `floor` = $floor WHERE `id` = $roomId ";
+
+  $result = $conn->query($sql);
+  if ($result) {
+    header("Location: $basePath/show/show.php?id=$roomId");
+  } else {
+    echo "KO";
+  }
+
+    $conn->close();
--- update/update.php
+++ update/update.php
@@ -1,2 +1,47 @@
 <?php
-include 'server.php';
+  include __DIR__ .'/../database.php';
+  include __DIR__ .'/../partials/header.php';
+
+  if (!empty($_GET['id'])) {
+    $roomId = $_GET['id'];
+  }
+
+  $sql= "SELECT * from `stanze` WHERE `id` = $roomId";
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    $room = $result->fetch_assoc();
+  } else {
+    die('id non esistente');
+  }
+
+ ?>
+
+ <div class="container">
+   <div class="row">
+     <div class="col-12">
+       <form action="server.php" method="post">
+         <div class="form-group">
+           <label for="room_nmber">Room Number</label>
+           <input class="form-control" type="text" name="room_number" value="<?php echo $room['room_number']; ?>">
+         </div>
+         <div class="form-group">
+           <label for="floor">Floor</label>
+           <input class="form-control" type="text" name="floor" value="<?php echo $room['floor']; ?>">
+         </div>
+         <div class="form-group">
+           <label for="beds">Beds</label>
+           <input class="form-control" type="text" name="beds" value="<?php echo $room['beds']; ?>">
+         </div>
+         <div class="form-group">
+           <input type="hidden" name="id" value="<?php echo $room['id']; ?>">
+           <input class="form-control" class="btn btn-submit" type="submit" value="Update">
+         </div>
+
+       </form>
+
+     </div>
+
+   </div>
+
+ </div>
